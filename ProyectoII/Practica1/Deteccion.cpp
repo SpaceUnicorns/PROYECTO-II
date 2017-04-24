895a72396280090f644ba3ec5f962da2926289f8
AlvaroErrepeBass <alvrod09@ucm.es>
2017-04-24 20:53:47 +0200

    Preparado el componente deteccion como comportamiento base de enemigo

--- ProyectoII/Practica1/Deteccion.cpp
+++ ProyectoII/Practica1/Deteccion.cpp
@@ -21,290 +21,316 @@ Deteccion::Deteccion(ObjetoJuego* entidad, float radio) :Componente(entidad), ra
 Deteccion::~Deteccion()
 {
 }
+// Comportamientos-----------------------------------------------------------------------------------------------------------------------------------------
 
-void Deteccion::update() {
-
-	enemy->setTarget(0);
-	setVista(dirAtaque); std::cout << dirAtaque << "\n";
-	switch (enemy->getEstado())
-	{
-	case Quieto:
-		cont++;
-		if (vagar > 0 && contVagar < 150){
-			Punto p = { enemy->getColisionBox().x, enemy->getColisionBox().y };
-			if (vagar == 1){
-				p.x -= 2;
-				if (static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->isColiding(p, info) != 1){
-					enemy->setAbsRect(-2, 0);
-					enemy->setRect(-2, 0);
-					dirAtaque = 6;
-				}
+void Deteccion::quieto(){
+	cont++;
+	if (vagar > 0 && contVagar < 150){
+		Punto p = { 0, 0 };
+		if (vagar == 1){
+			p.x -= 2;
+			if (static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->isColiding(p, info) != 1){
+				enemy->setAbsRect(-2, 0);
+				enemy->setRect(-2, 0);
+				dirAtaque = 6;
 			}
-			else if (vagar == 2){
-				p.x += 2;
-				if (static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->isColiding(p, info) != 1){
-					enemy->setAbsRect(2, 0);
-					enemy->setRect(2, 0);
-					dirAtaque = 2;
-				}
-			}
-			else if (vagar == 3){
-				p.y -= 1;
-				if (static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->isColiding(p, info) != 1){
-					enemy->setAbsRect(0, -1);
-					enemy->setRect(0, -1);
-					dirAtaque = 0;
-				}
-			}
-			else if (vagar == 4) {
-				p.y += 1;
-				if (static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->isColiding(p, info) != 1){
-					enemy->setAbsRect(0, 1);
-					enemy->setRect(0, 1);
-					dirAtaque = 4;
-				}
-			}
-			contVagar++;
-			static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->setRectBox(enemy->getRect().x - 5, enemy->getRect().y + 40);
-			if (contVagar >= 150){ vagar = 0; contVagar = 0; }
 		}
-		if (cont > 150){
-			rnd = (rand() % 11)+80;
-			if (rnd >80 && rnd < 89){
-				direccionAux = nullptr;
-				if (rnd >80 && rnd <83){ if (enemy->getAbsRect().x > 150)vagar = 1; }
-				else if (rnd >82 && rnd <85){ vagar = 2; }
-				else if (rnd >84 && rnd <87){ if (enemy->getAbsRect().y > 150)vagar = 3; }
-				else if (rnd >86 && rnd < 89){ vagar = 4; }
+		else if (vagar == 2){
+			p.x += 2;
+			if (static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->isColiding(p, info) != 1){
+				enemy->setAbsRect(2, 0);
+				enemy->setRect(2, 0);
+				dirAtaque = 2;
 			}
-			if (rnd >= 89){
-				//aulla
-			}
-			cont = 0;
 		}
-		if (cont > 50){
-			Punto cazP, recP;
-			cazP.x = enemy->getCazador()->getAbsRect().x; cazP.y = enemy->getCazador()->getAbsRect().y;
-			recP.x = enemy->getRecolector()->getAbsRect().x; recP.y = enemy->getRecolector()->getAbsRect().y;
-			if (inTriangle(vista, cazP) && inTriangle(vista, recP)){
-				float distRec, distCaz;
-				detectado = true;
-				recolectorIn(distRec);
-				cazadorIn(distCaz);
-				if (distRec < distCaz && enemy->getRecolector()->esDetectable()){
-					enemy->setTarget(1);
-					enemy->activaFollow();
-					enemy->setEstado(Moviendo);
-				}
-				else if (enemy->getCazador()->esDetectable()){
-					enemy->setTarget(0);
-					enemy->activaFollow();
-					enemy->setEstado(Moviendo);
-				}
-				cont = 0;
+		else if (vagar == 3){
+			p.y -= 1;
+			if (static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->isColiding(p, info) != 1){
+				enemy->setAbsRect(0, -1);
+				enemy->setRect(0, -1);
+				dirAtaque = 0;
 			}
-			else if (inTriangle(vista, cazP) && enemy->getCazador()->esDetectable()){
-				detectado = true;
-				enemy->setTarget(0);
-				enemy->activaFollow();
-				enemy->setEstado(Moviendo);
-				cont = 0;
+		}
+		else if (vagar == 4) {
+			p.y += 1;
+			if (static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->isColiding(p, info) != 1){
+				enemy->setAbsRect(0, 1);
+				enemy->setRect(0, 1);
+				dirAtaque = 4;
 			}
-			else if (inTriangle(vista, recP) && enemy->getRecolector()->esDetectable()){
-				detectado = true;
+		}
+		contVagar++;
+		static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->setRectBox(enemy->getRect().x - 5, enemy->getRect().y + 40);
+		if (contVagar >= 150){ vagar = 0; contVagar = 0; }
+	}
+	if (cont > 150){
+		rnd = (rand() % 11) + 80;
+		if (rnd >80 && rnd < 89){
+			direccionAux = nullptr;
+			if (rnd >80 && rnd <83){ if (enemy->getAbsRect().x > 150)vagar = 1; }
+			else if (rnd >82 && rnd <85){ vagar = 2; }
+			else if (rnd >84 && rnd <87){ if (enemy->getAbsRect().y > 150)vagar = 3; }
+			else if (rnd >86 && rnd < 89){ vagar = 4; }
+		}
+		if (rnd >= 89){
+			//aulla
+		}
+		cont = 0;
+	}
+	if (cont > 50){
+		Punto cazP, recP;
+		cazP.x = enemy->getCazador()->getAbsRect().x; cazP.y = enemy->getCazador()->getAbsRect().y;
+		recP.x = enemy->getRecolector()->getAbsRect().x; recP.y = enemy->getRecolector()->getAbsRect().y;
+		if (inTriangle(vista, cazP) && inTriangle(vista, recP)){
+			float distRec, distCaz;
+			detectado = true;
+			recolectorIn(distRec);
+			cazadorIn(distCaz);
+			if (distRec < distCaz && enemy->getRecolector()->esDetectable()){
 				enemy->setTarget(1);
 				enemy->activaFollow();
 				enemy->setEstado(Moviendo);
-				cont = 0;
 			}
-			else detectado = false;
-		}
-		// Al cabo de un tiempo vuelve a casa
-		float distCasa;
-		compruebaRadio(enemy->getCasita()->getAbsRect(), distCasa);
-		if ( distCasa > 1000){
+			else if (enemy->getCazador()->esDetectable()){
+				enemy->setTarget(0);
+				enemy->activaFollow();
+				enemy->setEstado(Moviendo);
+			}
 			cont = 0;
-			enemy->desactivaFollow();
-			enemy->setEstado(Volviendo);
+		}
+		else if (inTriangle(vista, cazP) && enemy->getCazador()->esDetectable()){
+			detectado = true;
+			enemy->setTarget(0);
+			enemy->activaFollow();
+			enemy->setEstado(Moviendo);
+			cont = 0;
+		}
+		else if (inTriangle(vista, recP) && enemy->getRecolector()->esDetectable()){
+			detectado = true;
+			enemy->setTarget(1);
+			enemy->activaFollow();
+			enemy->setEstado(Moviendo);
+			cont = 0;
+		}
+		else detectado = false;
+	}
+	// Al cabo de un tiempo vuelve a casa
+	float distCasa;
+	compruebaRadio(enemy->getCasita()->getAbsRect(), distCasa);
+	if (distCasa > 1000){
+		cont = 0;
+		enemy->desactivaFollow();
+		enemy->setEstado(Volviendo);
+	}
+}
+void Deteccion::movimiento(){
+	dirAtaque = enemy->getDir();
+	float distRec;
+	recolectorIn(distRec);
+	float distCaz;
+	cazadorIn(distCaz);
+	cont++;
+	if (cont > 100)
+	{
+		acechar();
+		cont = 0;
+	}
+	if (enemy->getTarget() == enemy->getRecolector() && enemy->getRecolector()->esDetectable()){
+		dirAtaque = enemy->getDir();
+		setVista(dirAtaque);
+		if (distRec <= 80){
+			preparaAtaque(1);
+			enemy->setEstado(Atacando);
+		}
+	}
+	else if (enemy->getTarget() == enemy->getCazador() && enemy->getCazador()->esDetectable()){
+		if (distCaz <= 80){
+			preparaAtaque(0);
+			enemy->setEstado(Atacando);
+		}
+	}
+}
+void Deteccion::volviendo(){
+	cont++;
+	if (cont >= 500){
+		cont = 0;
+		dirAtaque = enemy->getDir();
+		enemy->setEstado(Quieto);
+	}
+}
+void Deteccion::atacando(){
+	if (cont < 100)
+		cont++;
+	else { enemy->setEstado(PostAtaque); cont = 0; }
+	switch (dirAtaque)
+	{
+	case 4:
+		if (cont < 100){
+			enemy->setAbsRect(0, -1);
+			enemy->setRect(0, -1);
+			ultAtaque = 0;
 		}
 		break;
-	case Moviendo:
-		dirAtaque = enemy->getDir();
+	case 5:
+		if (cont < 100){
+			enemy->setAbsRect(2, -1);
+			enemy->setRect(2, -1);
+			ultAtaque = 1;
+		}
+		break;
+	case 6:
+		if (cont < 100){
+			enemy->setAbsRect(2, 0);
+			enemy->setRect(2, 0);
+			ultAtaque = 2;
+		}
+		break;
+	case 7:
+		if (cont < 100){
+			enemy->setAbsRect(2, 1);
+			enemy->setRect(2, 1);
+			ultAtaque = 3;
+		}
+		break;
+	case 0:
+		if (cont < 100){
+			enemy->setAbsRect(0, 1);
+			enemy->setRect(0, 1);
+			ultAtaque = 4;
+		}
+		break;
+	case 1:
+		if (cont < 100){
+			enemy->setAbsRect(-2, 1);
+			enemy->setRect(-2, 1);
+			ultAtaque = 5;
+		}
+		break;
+	case 2:
+		if (cont < 100){
+			enemy->setAbsRect(-2, 0);
+			enemy->setRect(-2, 0);
+			ultAtaque = 6;
+		}
+		break;
+	case 3:
+		if (cont < 100){
+			enemy->setAbsRect(-2, -1);
+			enemy->setRect(-2, -1);
+			ultAtaque = 7;
+		}
+		break;
+	}
+	static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->setRectBox(enemy->getRect().x - 5, enemy->getRect().y + 40);
+}
+void Deteccion::postAtaque(){
+	direccionAux = nullptr;
+	if (cont == 0){ // Segun adonde atacara el lobo da la vuelta a una posicion u otra
+		rnd = rand() % 2;
+		if (ultAtaque == 0){
+			if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x + 100, enemy->getAbsRect().y); dirAtaque = 5; }
+			else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x - 100, enemy->getAbsRect().y); dirAtaque = 3; }
+		}
+		else if (ultAtaque == 1){
+			if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x - 100, enemy->getAbsRect().y); dirAtaque = 4; }
+			else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y + 100); dirAtaque = 6; }
+		}
+		else if (ultAtaque == 2){
+			if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y + 100); dirAtaque = 5; }
+			else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y - 100); dirAtaque = 7; }
+		}
+		else if (ultAtaque == 3){
+			if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x - 100, enemy->getAbsRect().y); dirAtaque = 0; }
+			else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y - 100); dirAtaque = 6; }
+		}
+		else if (ultAtaque == 4){
+			if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x + 100, enemy->getAbsRect().y); dirAtaque = 7; }
+			else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x - 100, enemy->getAbsRect().y); dirAtaque = 1; }
+		}
+		else if (ultAtaque == 5){
+			if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x + 100, enemy->getAbsRect().y); dirAtaque = 0; }
+			else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y - 100); dirAtaque = 2; }
+		}
+		else if (ultAtaque == 6){
+			if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y + 100); dirAtaque = 1; }
+			else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y - 100); dirAtaque = 3; }
+		}
+		else if (ultAtaque == 7){
+			if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x + 100, enemy->getAbsRect().y); dirAtaque = 4; }
+			else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y + 100); dirAtaque = 2; }
+		}
+		enemy->followThis(direccionAux);
+	}
+	else if (cont > 100) {
 		float distRec;
 		recolectorIn(distRec);
 		float distCaz;
 		cazadorIn(distCaz);
-		cont++;
-		if (cont > 100)
-		{
-			acechar();
-			cont = 0;
+		enemy->setTarget(0);
+		delete direccionAux;
+
+		if (distCaz <= 80){
+			preparaAtaque(0);
+			enemy->setEstado(Atacando);
 		}
-		if (enemy->getTarget() == enemy->getRecolector() && enemy->getRecolector()->esDetectable()){
-			dirAtaque = enemy->getDir();
-			setVista(dirAtaque);
-			if (distRec <= 80){
-				preparaAtaque(1);
-				enemy->setEstado(Atacando);
-			}
+		else if (distRec <= 80){
+			preparaAtaque(0);
+			enemy->setEstado(Atacando);
 		}
-		else if (enemy->getTarget() == enemy->getCazador() && enemy->getCazador()->esDetectable()){
-			if (distCaz <= 80){
-				preparaAtaque(0);
-				enemy->setEstado(Atacando);
-			}
-		}
+		else enemy->setEstado(Quieto);
+		cont = 0;
+	}
+	cont++;
+}
+void Deteccion::atrapado(){
+	cont++;
+	if (cont >= 500)
+	{
+		cont = 0;
+		enemy->setEstado(Quieto);
+	}
+}
+void Deteccion::herido(){		
+	//Animacion Herido
+	if (enemy->life < 0)
+		enemy->setEstado(Muerto);
+	else if (cont > 100){
+		cont = 0;
+		enemy->setEstado(Quieto);
+	}
+}
+void Deteccion::muerto(){}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+void Deteccion::update() {
+
+	enemy->setTarget(0);
+	setVista(dirAtaque);
+	switch (enemy->getEstado())
+	{
+	case Quieto:
+		quieto();
+		break;
+	case Moviendo:
+		movimiento();
 		break;
 	case Volviendo:
-		cont++;
-		if (cont >= 500){
-			cont = 0;
-			dirAtaque = enemy->getDir();
-			enemy->setEstado(Quieto);
-		}
+		volviendo();
 		break;
 	case Atacando:
-		if (cont < 100)
-			cont++;
-		else { enemy->setEstado(PostAtaque); cont = 0; }
-		switch (dirAtaque)
-		{
-		case 4:
-			if (cont < 100){
-				enemy->setAbsRect(0, -1);
-				enemy->setRect(0, -1);
-				ultAtaque = 0;
-			}
-			break;
-		case 5:
-			if (cont < 100){
-				enemy->setAbsRect(2, -1);
-				enemy->setRect(2, -1);
-				ultAtaque = 1;
-			}
-			break;
-		case 6:
-			if (cont < 100){
-				enemy->setAbsRect(2, 0);
-				enemy->setRect(2, 0);
-				ultAtaque = 2;
-			}
-			break;
-		case 7:
-			if (cont < 100){
-				enemy->setAbsRect(2, 1);
-				enemy->setRect(2, 1);
-				ultAtaque = 3;
-			}
-			break;
-		case 0:
-			if (cont < 100){
-				enemy->setAbsRect(0, 1);
-				enemy->setRect(0, 1);
-				ultAtaque = 4;
-			}
-			break;
-		case 1:
-			if (cont < 100){
-				enemy->setAbsRect(-2, 1);
-				enemy->setRect(-2, 1);
-				ultAtaque = 5;
-			}
-			break;
-		case 2:
-			if (cont < 100){
-				enemy->setAbsRect(-2, 0);
-				enemy->setRect(-2, 0);
-				ultAtaque = 6;
-			}
-			break;
-		case 3:
-			if (cont < 100){
-				enemy->setAbsRect(-2, -1);
-				enemy->setRect(-2, -1);
-				ultAtaque = 7;
-			}
-			break;
-		}
-		static_cast<ColisionBox*>(enemy->dameComponente("ColisionBox"))->setRectBox(enemy->getRect().x - 5, enemy->getRect().y + 40);
+		atacando();
 		break;
 	case Atrapado:
-		cont++;
-		if (cont >= 500)
-		{
-			cont = 0;
-			enemy->setEstado(Quieto);
-		}
+		atrapado();
 		break;
 	case PostAtaque:
-		direccionAux = nullptr;
-		if (cont == 0){ // Segun adonde atacara el lobo da la vuelta a una posicion u otra
-			rnd = rand() % 2;
-			if (ultAtaque == 0){
-				if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x + 100, enemy->getAbsRect().y); dirAtaque = 5; }
-				else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x - 100, enemy->getAbsRect().y); dirAtaque = 3; }
-			}
-			else if (ultAtaque == 1){
-				if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x - 100, enemy->getAbsRect().y); dirAtaque = 4; }
-				else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y + 100); dirAtaque = 6; }
-			}
-			else if (ultAtaque == 2){
-				if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y + 100); dirAtaque = 5; }
-				else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y - 100); dirAtaque = 7; }
-			}
-			else if (ultAtaque == 3){
-				if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x - 100, enemy->getAbsRect().y); dirAtaque = 0; }
-				else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y - 100); dirAtaque = 6; }
-			}
-			else if (ultAtaque == 4){
-				if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x + 100, enemy->getAbsRect().y); dirAtaque = 7; }
-				else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x - 100, enemy->getAbsRect().y); dirAtaque = 1; }
-			}
-			else if (ultAtaque == 5){
-				if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x + 100, enemy->getAbsRect().y); dirAtaque = 0; }
-				else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y - 100); dirAtaque = 2; }
-			}
-			else if (ultAtaque == 6){
-				if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y + 100); dirAtaque = 1; }
-				else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y - 100); dirAtaque = 3; }
-			}
-			else if (ultAtaque == 7){
-				if (rnd == 0){ delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x + 100, enemy->getAbsRect().y); dirAtaque = 4; }
-				else { delete direccionAux; direccionAux = new ObjetoPG(enemy->getPJuego(), enemy->getAbsRect().x, enemy->getAbsRect().y + 100); dirAtaque = 2; }
-			}
-			enemy->followThis(direccionAux);
-		}
-		else if (cont > 100) {
-			float distRec;
-			recolectorIn(distRec);
-			float distCaz;
-			cazadorIn(distCaz);
-			enemy->setTarget(0);
-			delete direccionAux;
-
-			if (distCaz <= 80){
-				preparaAtaque(0);
-				enemy->setEstado(Atacando);
-			}
-			else if (distRec <= 80){
-				preparaAtaque(0);
-				enemy->setEstado(Atacando);
-			}
-			else enemy->setEstado(Quieto);
-			cont = 0;
-		}
-		cont++;
+		postAtaque();
 		break;
 	case Herido:
-		//Animacion Herido
-		if (enemy->life < 0)
-			enemy->setEstado(Muerto);
-		else if (cont > 100){
-			cont = 0;
-			enemy->setEstado(Quieto);
-		}
+		herido();
 		break;
 	case Muerto:
+		muerto();
 		break;
 	}
 	/*
