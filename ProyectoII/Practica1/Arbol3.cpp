56f7bc7960fd65ae5bfa16da9f637c5d0860ed8b
Antonio Cardona Costa <antocard@ucm.es>
2017-05-08 19:51:54 +0200

    Agregada clase Arbol3

--- ProyectoII/Practica1/Arbol3.cpp
+++ ProyectoII/Practica1/Arbol3.cpp
@@ -1,16 +1,16 @@
-#include "Arbol2.h"
+#include "Arbol3.h"
 #include "ColisionBox.h"
 #include "Nivel1.h"
 #include <SDL.h>
 
 
-Arbol2::Arbol2(juegoPG * juego, int px, int py) : ObjetoPG(juego, px, py)
+Arbol3::Arbol3(juegoPG * juego, int px, int py) : ObjetoPG(juego, px, py)
 {
 	nombre.push_back("1");
-	nombre.push_back("Arbol2");
+	nombre.push_back("Arbol3");
 
-	et = TArbol2;
-	interactuable = false;
+	et = TArbol3A;
+	interactuable = true;	//De este arbol se recogen enredaderas
 	rect.w = 100;
 	rect.h = 200;
 	absRect.w = rect.w;
@@ -21,12 +21,12 @@ Arbol2::Arbol2(juegoPG * juego, int px, int py) : ObjetoPG(juego, px, py)
 }
 
 
-Arbol2::~Arbol2()
+Arbol3::~Arbol3()
 {
 
 }
 
-void Arbol2::draw() { //BORRAR CUANDO NO SEA NECESARIO VER EL BOX COLLIDER;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+void Arbol3::draw() { //BORRAR CUANDO NO SEA NECESARIO VER EL BOX COLLIDER;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	aux = (dynamic_cast<EstadoPG*>(pJuego->estados.top())->getCamara());
 	rect.x -= aux.x;
 	rect.y -= aux.y;
