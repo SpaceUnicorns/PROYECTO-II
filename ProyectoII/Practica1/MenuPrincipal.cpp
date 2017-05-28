c9f6dd9228d4a8b1264c7e79d4cc831c51f0a39b
AlvaroErrepeBass <alvrod09@ucm.es>
2017-05-28 16:05:35 +0200

    Nivel cueva con colisiones mejor

--- ProyectoII/Practica1/MenuPrincipal.cpp
+++ ProyectoII/Practica1/MenuPrincipal.cpp
@@ -60,8 +60,8 @@ void MenuPrincipal::update(int delta)
 			//pJuego->estados.push(new Tutorial2(pJuego, "../docs/mapa1.txt", "../docs/objetosTutorial.txt", rec, caz));
 
 			//IR AL NIVEL 1 6850, 9150
-			Punto caz; caz.x = 6950; caz.y = 9150; Punto rec; rec.x = 7010; rec.y = 9130;
-			pJuego->estados.push(new Nivel1(pJuego, "../docs/mapa.txt", "../docs/objetosNivel1.txt", rec, caz, "R","../sounds/reverb/ReverbBosque.wav"));
+			/*Punto caz; caz.x = 6950; caz.y = 9150; Punto rec; rec.x = 7010; rec.y = 9130;
+			pJuego->estados.push(new Nivel1(pJuego, "../docs/mapa.txt", "../docs/objetosNivel1.txt", rec, caz, "R","../sounds/reverb/ReverbBosque.wav"));*/
 
 			//IR AL NIVEL 2
 			/*Punto caz; caz.x = 2723; caz.y = 2394; Punto rec; rec.x = 2603; rec.y = 2394;
@@ -70,8 +70,8 @@ void MenuPrincipal::update(int delta)
 			pJuego->estados.push(new Nivel2(pJuego, "../docs/mapa2.txt", "../docs/objetosNivel2.txt", rec, caz, "R"));*/
 
 			//IR AL NIVEL 3
-			/*Punto caz; caz.x = 3456; caz.y = 696; Punto rec; rec.x = 3496; rec.y = 726;
-			pJuego->estados.push(new Nivel3(pJuego, "../docs/mapa3.txt", "../docs/objetosNivel3.txt", rec, caz, "R"));*/
+			Punto caz; caz.x = 3456; caz.y = 696; Punto rec; rec.x = 3496; rec.y = 726;
+			pJuego->estados.push(new Nivel3(pJuego, "../docs/mapa3.txt", "../docs/objetosNivel3.txt", rec, caz, "R"));
 			delete borrar;
 		}
 		else if (opcion == 3){
