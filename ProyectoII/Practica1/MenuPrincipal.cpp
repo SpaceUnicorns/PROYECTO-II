50a932dfac0fe30d060fe21b35bd896396c60e49
Usuario Local <Usuario Local>
2017-05-29 18:44:18 +0200

    Lyov no equipa el hacha

--- ProyectoII/Practica1/MenuPrincipal.cpp
+++ ProyectoII/Practica1/MenuPrincipal.cpp
@@ -52,8 +52,8 @@ void MenuPrincipal::update(int delta)
 			getPJuego()->estados.pop();
 			
 			//IR A TUTORIAL 1
-			Punto rec; rec.x = 6925; rec.y = 10265; Punto caz; caz.x = 0; caz.y = 0;
-			getPJuego()->estados.push(new Tutorial(getPJuego(), "../docs/mapa1.txt", "../docs/objetosTutorial.txt", rec, caz));
+			//Punto rec; rec.x = 6925; rec.y = 10265; Punto caz; caz.x = 0; caz.y = 0;
+			//getPJuego()->estados.push(new Tutorial(getPJuego(), "../docs/mapa1.txt", "../docs/objetosTutorial.txt", rec, caz));
 			
 			//IR A TUTORIAL 2
 			//Punto rec; rec.x = 0; rec.y = 0; Punto caz; caz.x = 6925; caz.y = 10260;
@@ -61,9 +61,10 @@ void MenuPrincipal::update(int delta)
 
 			//IR AL NIVEL 1 6850, 9150
 			//Punto caz; caz.x = 6950; caz.y = 9150; Punto rec; rec.x = 7010; rec.y = 9130;
+			Punto caz; caz.x = 1778; caz.y = 3576; Punto rec; rec.x = 2078; rec.y = 3596;
 			//====== FINAL DEL NIVEL ======= 3077 , 1260
 			//Punto caz; caz.x = 3090; caz.y = 1420; Punto rec; rec.x = 3120; rec.y = 1420;
-			//pJuego->estados.push(new Nivel1(pJuego, "../docs/mapa.txt", "../docs/objetosNivel1.txt", rec, caz, "R","../sounds/reverb/ReverbBosque.wav"));
+			pJuego->estados.push(new Nivel1(pJuego, "../docs/mapa.txt", "../docs/objetosNivel1.txt", rec, caz, "R","../sounds/reverb/ReverbBosque.wav"));
 
 			//IR AL NIVEL 2
 			/*Punto caz; caz.x = 2723; caz.y = 2394; Punto rec; rec.x = 2603; rec.y = 2394;
